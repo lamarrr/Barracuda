c9111ad72db794b635ccb28943ea785b751de271
Basit Ayantunde <rlamarrr@gmail.com>
2019-12-01 22:22:37 +0100

    Delete fusion_bm.sh

--- fusion_bm.sh
+++
@@ -1,4 +0,0 @@
-nvcc barracuda/fusion/fusion.cu -c -o fusion_device.o -I. --expt-extended-lambda
-clang++ barracuda/fusion/fusion_bm.cc -c -o fusion_host.o -I.
-clang++ fusion_host.o fusion_device.o -o fusion.o -lcudart -lbenchmark -pthread
-./fusion.o
